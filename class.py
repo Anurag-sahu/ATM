03f98dcd78abf422b68db73e642a33e06819e51c
Anurag-sahu <73108926+Anurag-sahu@users.noreply.github.com>
2021-07-29 09:28:48 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class ATM(object):
+    def __init__(self,money,PIN,Account):
+        self.money=money
+        self.PIN=PIN
+        self.Account=Account
+    def get_money(self):
+        print("Trasnisting Money")
+     
+    def PIN(self):
+        print("****")
+
+
+volk=ATM("WBA", "greay","AC12AN34S")
